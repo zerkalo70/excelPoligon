a7aec1d6d01bd97cb1742c40477de94b834a46fe
Zerkalo70 <halygali@mail.ru>
2021-09-07 02:15:46 +0300

    Finish css layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import 'core-js/stable' // Вместо @babel/polyfill
 import 'regenerator-runtime/runtime' // Вместо @babel/polyfill
 import './scss/index.scss';
-import './module';
 
 console.log('Hello from index.js');
